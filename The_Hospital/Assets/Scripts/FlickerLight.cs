d6ec28e4ae4ed79f3c5ad2bcc648be7b36279e01
al-han <alexhan168@gmail.com>
2024-11-30 22:29:26 -0800

    Updated Light Flicker and got new wood texture

--- The_Hospital/Assets/Scripts/FlickerLight.cs
+++ The_Hospital/Assets/Scripts/FlickerLight.cs
@@ -1,32 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlickerLight : MonoBehaviour
 {
     private bool isFlickering = false;
     public float timeDelay;
+    
+    public float minRangeDelay =.01f;
+    public float maxRangeDelay = .2f;
+
+    public float minLight = 1f;
+
+    public float maxLight =2f;
+
+
+    public float minRange = 1f;
+    public float maxRange =10f;
+
+
+
     // Start is called before the first frame update
     // Update is called once per frame
     void Update()
     {
         if(isFlickering == false) {
+            
+            StartCoroutine(changeLightInstense());
+            StartCoroutine(changeRange());
             StartCoroutine(Flicker());
         }
     }
     IEnumerator Flicker() {
+
         isFlickering = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-
-        timeDelay = Random.Range(.01f,.2f);//change the float values to determine how often is the delay
+        
+        timeDelay = Random.Range(minRangeDelay,maxRangeDelay);//change the float values to determine how often is the delay
 
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true; // same here
-        timeDelay = Random.Range(0.1f, .2f);
+        timeDelay = Random.Range(minRangeDelay, maxRangeDelay);
         yield return new WaitForSeconds(timeDelay);
         isFlickering=false;
     }
     //since isFlickering switches back and forth
     // got it here https://www.youtube.com/watch?v=JuvY0fYVPM0
-    // note script doesn't worked for baked lights, mixed seem to work here
+    // note script doesn't worked for baked lights
+    IEnumerator changeLightInstense() {
+      Light currentLight = this.gameObject.GetComponent<Light>();
+
+      float genLight=Random.Range(minLight,maxLight);
+      //yield return new WaitForSeconds(timeDelay);
+      //this.gameObject.GetComponent;
+      currentLight.intensity = genLight;
+      yield return new WaitForSeconds(timeDelay);
+      
+    }
+    IEnumerator changeRange() {
+        Light currentLight = this.gameObject.GetComponent<Light>();
+        float changeRange = Random.Range(minRange,maxRange);
+        currentLight.range = changeRange;
+        yield return new WaitForSeconds(timeDelay);
+
+    }
 }
