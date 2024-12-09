c64b3919dcf0ecbb405fc1c2f1602bb66fae4791
Allen W. Zhang <136660147+azhang175@users.noreply.github.com>
2024-12-09 14:10:50 -0800

    Redid the inventory

--- The_Hospital/Assets/Scripts/Player2.cs
+++ The_Hospital/Assets/Scripts/Player2.cs
@@ -2,28 +2,42 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
     [SerializeField] private GameObject _playerGo;
-    [SerializeField] private InteractionComponent _interaction;
     [SerializeField] private float speed;
-
-    //public InventoryObject inventory;
+    
     private InputComponent _input;
     private PhysicsComponent _physics;
-    
     private Vector3 inputDirection;
+
+    public GameObject HUD;
+    public InventoryManager inventoryManager;
+    private ItemPickup _itemPickUP;
+
 
     private void Start()
     {
         _input = GetComponent<InputComponent>();
         _physics = GetComponent<PhysicsComponent>();
+        _itemPickUP = GetComponent<ItemPickup>();
     }
     
     void Update()
     {
          inputDirection = _input.UpdateInput(); // Player input
         // Pass in movement direction to Sprite
+
+        //Check if the "H" key is pressed
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            HUD.SetActive(!HUD.activeSelf);
+
+            if (HUD.activeSelf)
+            {
+                inventoryManager.ListItems();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -32,4 +46,14 @@ public class Player : MonoBehaviour
         Vector3 localInputDirection = transform.TransformDirection(inputDirection);
         _physics.MoveEntity(localInputDirection, speed); // Player Physics
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ItemPickup item = other.GetComponent<ItemPickup>();
+        if (item != null)
+        {
+            item.Pickup();
+        }
+    }
+
 }
