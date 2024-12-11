1e553aebf42c3de329af391e661246445c6bb725
BrightlordGwyn <tamangyunis@gmail.com>
2024-12-11 01:02:13 -0800

    door script with KEY

--- The_Hospital/Assets/Scripts/Interactions/Door.cs
+++ The_Hospital/Assets/Scripts/Interactions/Door.cs
@@ -8,30 +8,48 @@ public class Door : MonoBehaviour, IInteractible
     [SerializeField] private Animator theDoor = null;
     
     private bool _isOpen = false;
-    
+    public int doorID;
+
+    public InventoryManager inventoryManager;
     public string InteractionPrompt => prompt;
     
     public bool Interact(Interactor interactor)
     {
         if (!_isOpen)
         {
-            // Play animation
-            // Consume key
-            // getComponent Inventory
-            // if inventory == null, return false
-            theDoor.Play("open door", 0 , 0.0f);
+             InventoryManager inventory = interactor.GetComponent<InventoryManager>();
+            if(inventory == null)
+            {
+                Debug.Log("No inventory found");
+                return false;
+            }
+
+            foreach (Item item in inventory.Items)
+            {
+                if(item.itemType == ItemType.Key && item.keyId == doorID)
+                {
+                    theDoor.Play("open door", 0 , 0.0f);
+                    Debug.Log("Opening Door");
+                    _isOpen = true;
+                    //Consume Key
+                    inventory.Items.Remove(item);
+                    Debug.Log("Key removed from inventory");
+                    // Play animation
+                    return true;
+
+                }
+                else
+                {
+                    Debug.Log("No key in inventory");
+                    return false;
+                }
+                
+            }
             Debug.Log("Opening Door");
             _isOpen = true;
-                // return false;
-            // else, no key found
-                // Debug.Log("No key in inventory");
-                // return false;
+            } else { 
             
-        } else { 
-            //Debug.Log("Door is already open");
-            theDoor.Play("close door", 0, 0.0f);
-            Debug.Log("Closing Door");
-            _isOpen = false;
+            Debug.Log("Door is already open");
         }
         
         return true;
